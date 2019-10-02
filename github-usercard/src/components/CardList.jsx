cef3dfc88742c205ae40af44c74c52e6d947b46f
Dom <domeccleston@gmail.com>
2019-10-02 07:58:46 +0100

    refactored so that code actually works to mvp but can't do any more for now as i'm being rate limited

--- github-usercard/src/components/CardList.jsx
+++ github-usercard/src/components/CardList.jsx
@@ -1,61 +1,69 @@
-import React from 'react';
-import axios from 'axios';
-import UserCard from './Card';
+import React from "react";
+import axios from "axios";
+import UserCard from "./UserCard";
 
 export default class CardList extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            userData: [],
-            followersData: [],
-            searchText: "",
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: [],
+      followersData: [],
+      searchText: ""
+    };
+  }
 
-    setSearchText = () => event => {
-        this.setState({
-            searchText: event.target.value
-        })
-    }
+  setSearchText = () => event => {
+    this.setState({
+      searchText: event.target.value
+    });
+  };
 
-    componentDidMount() {
-        axios.get('http://api.github.com/users/domeccleston')
-            .then(res => 
-                this.setState({
-                    userData: res.data
-                }))
-            .then(res => 
-                axios.get(this.state.userData.followers_url)
-                    .then(res => {
-                        res.data.forEach((follower) => {
-                            axios.get(follower.url)
-                                .then(res => {
-                                    this.setState(currentState => {
-                                        
-                                    }
-                                })
-                        })
-                    })
-                )
-    }
+  getGithubData = user => {
+    axios.get(`https://api.github.com/users/${user}`).then(res => {
+      this.setState({
+        userData: res.data,
+        followersData: []
+      });
+    });
+  };
 
-    handleSubmit = () => event => {
-        event.preventDefault();
-    }
+  getGithubFollowersData = user => {
+    axios.get(`https://api.github.com/users/${user}/followers`).then(res => {
+      res.data.forEach(follower => {
+        axios
+          .get(`https://api.github.com/users/${follower.login}`)
+          .then(res => {
+            console.log(res.data);
+            this.state.followersData.concat(res.data);
+          })
+          .catch(err => console.log(err));
+      });
+    });
+  };
 
+  componentDidMount() {
+    this.getGithubData("domeccleston");
+    this.getGithubFollowersData("domeccleston");
+  }
 
-    render() {
-        console.log(this.state.followersData)
-        return (
-            <div>
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="search" onChange={this.setSearchText}/>
-            </form>
+  handleSubmit = () => event => {
+    event.preventDefault();
+  };
 
-            {this.state.followersData.map(follower => (
-                <UserCard userData={follower} />
-            ))}
-            </div>
-        )
-    }
+  render() {
+    console.log(this.state.followersData);
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="search"
+            onChange={this.setSearchText}
+          />
+        </form>
+
+        <UserCard userData={this.state.userData} />
+      </div>
+    );
+  }
 }
